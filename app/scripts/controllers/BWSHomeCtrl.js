f2778f0db8ec71f785e0cc40e481ef1425b8fbc7
676987 <676987@01HW893892.India.TCS.com>
2016-01-13 13:43:28 +0530

    Initial commit

---
+++ app/scripts/controllers/BWSHomeCtrl.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name BWSApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the BWSApp
+ */
+angular.module('BWSApp')
+  .controller('BWSHomeCtrl', ['$scope',function ($scope) {
+     
+  }]);
